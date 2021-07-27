5358d5f4b25ddf33bbe2926566d40465b0f52316
SteveJayH <jay0118@yonsei.ac.kr>
2021-07-27 20:58:33 +0900

    video

--- README.md
+++ README.md
@@ -1,5 +1,85 @@
-# 3DM
-Official code for 3DM.
+<h2 align="center">3DM: Deep decomposition and deconvolution microscopy</h2>
+
+<p align="center">
+<img width="60%" src="src/Visualization1_resize75.gif">
+</p>
+<h6 align="center">Demo video acquired using 3DM.
+
+ Compression is applied. See Supplementary video 1.
+ </h6>
+
+Official source codes for "3DM: Deep decomposition and deconvolution microscopy", Optics Express.
 
 
-/media/user/HDD2_3DM/3DM
+## Running BEAR
+
+You can run BEAR which does Robust PCA by very simple code.
+Just download or clone this repository and use the code below.
+`--D` refers the name of the data and `--d` enables the default settings of several hyperparameters.
+
+```bash
+python scripts/run_BEAR.py --D hall --d True
+```
+
+Results will be saved in `results/BEAR/hall_(Y/L/S)` directory.
+
+## Hardware specification & Requirements
+
+**Hardware specification**
+
+```markdown
+OS : Ubuntu 18.0.4
+CPU : Intel Xeon Silver 4214 CPU @ 2.20GHz
+GPU : GeForce RTX 2080 Ti 11GB
+RAM : 128GM
+```
+
+**Requirements (May be okay with different versions.)**
+```markdown
+torch==1.7.0
+numpy==1.19.4
+psutil==5.7.2
+
+scipy==1.5.3
+scikit-image==0.17.2
+mat73==0.46
+```
+
+## Reproduce the paper
+
+We have already added small surveillance video, and calcium imaging data of mouse which are widely used in RPCA paper.
+You can first try these data.
+Calcium imaging data of zebrafish we have used in paper is in this [Google Drive](https://drive.google.com/file/d/115lCnwIVU0TtKedQ_31FDaOG8wksGmG9/view?usp=sharing) (~10GB).
+Download, unzip, and move .tif and .mat files inside the data folder. 
+
+1. Figure 3. Phase diagram.
+```bash
+python scripts/phase_diagram.py --D None --d None
+```
+
+2. Table 1. and Figure 4. Decomposition of the zebrafish caclium imaging data.
+```bash
+python scripts/run_Greedy_BEAR.py --D zebrafish_150 --d True
+python scripts/run_Greedy_BEAR.py --D zebrafish_1000 --d True
+```
+Due to the size of data, loading files itself does take long time (Minutes in HDD).
+And for 1000 length video, about 100GB of RAM is required.
+
+3. Figure 5. and Figure 6. Cascaded BEAR for analysis of neuronal activity.
+```bash
+python scripts/run_Cascaded_BEAR.py --D demoMovie --d True
+python scripts/run_Cascaded_BEAR.py --D spinning_confocal --d True
+```
+For accuracy, number of epochs in default settings of Cascaded BEAR is very large.
+Can be observed that loss value does not decrease actually after small number of epochs.
+You can reduce the `args.epoch` if you want.
+
+## Citation
+```markdown
+@inproceedings{han2021efficient,
+  title={Efficient neural network approximation of robust PCA for automated analysis of calcium imaging data},
+  author={Seungjae Han and Eun-Seo Cho and Inkyu Park and Kijung Shin and Young-Gyu Yoon},
+  booktitle={International Conference on Medical Image Computing and Computer Assisted Intervention},
+  year={2021}
+}
+```
---
+++ codes/unet3d/unet_model.py
@@ -0,0 +1,95 @@
+""" Full assembly of the parts to form the complete network """
+import torch.nn as nn
+
+import sys
+sys.path.append(".")
+from src.unet3d.unet_parts import *
+
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, 
+                 depth, channel,
+                 normalization=None,
+                 activation='lrelu',
+                 bias=True):
+        super(UNet, self).__init__()
+
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.depth = depth
+        self.channel = channel
+
+        if type(channel) is int:
+            channels = []
+            for i in range(depth + 1):
+                channels.append(channel * pow(2, i))
+        
+        self.incblock = []
+        self.incblock.append(Conv(n_channels, channels[0], kernel_size=3, bias=bias,
+                                        normalization=None,
+                                        activation=activation))
+
+        z_true_idx = 0
+        if depth > 1:
+            for i in range(depth - 1):
+                self.incblock.append(Down(channels[i], channels[i+1],
+                                          kernel_size=3, bias=bias,
+                                          normalization=normalization,
+                                          activation=activation,
+                                          z_true=bool(z_true_idx)))
+                z_true_idx = 1 - z_true_idx
+        self.incblock.append(Down(channels[-2], channels[-1],
+                                  kernel_size=3, bias=True,
+                                  normalization=normalization,
+                                  activation=activation,
+                                  z_true=bool(z_true_idx)))
+        self.incblockMod = nn.ModuleList(self.incblock)
+
+        self.decblock = []
+        if depth > 1:
+            for i in range(depth - 1):
+                self.decblock.append(Up(channels[-i-1], channels[-i-2],
+                                        kernel_size=3, bias=bias,
+                                        normalization=normalization,
+                                        activation=activation,
+                                        z_true=bool(z_true_idx)))
+                z_true_idx = 1 - z_true_idx
+        self.decblock.append(Up(channels[1], channels[0],
+                                kernel_size=3, bias=bias,
+                                normalization=normalization,
+                                activation=activation,
+                                z_true=bool(z_true_idx)))
+        self.decblock.append(OutConv(channels[0], n_classes,
+                                     kernel_size=3, bias=None,
+                                     normalization=normalization,
+                                     activation=activation))
+
+        self.decblockMod = nn.ModuleList(self.decblock)
+
+    def forward(self, x):
+        # x, pads = self.pad_to(x, int(math.pow(2, self.depth)))
+
+        xs = []
+        xs.append(x)
+        for mod in self.incblock:
+            x = mod(x)
+            xs.append(x)
+
+        x = self.decblock[0](xs[-1], xs[-2])
+
+        for i in range(self.depth - 1):
+            x = self.decblock[i + 1](x, xs[-3-i])
+        output = self.decblock[-1](x)
+
+        return output
+
+
+
+if __name__ == "__main__":
+    net = UNet(n_channels=1, n_classes=1, depth=4, channel=1,
+               normalization=None, activation='lrelu').cuda()
+    import torchsummary
+
+    torchsummary.summary(net, (1, 128, 128, 64))
+    print(net)
