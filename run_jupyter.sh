9872efdf47c1636f19e80a88f8b3d0a6409859b5
SteveJayH <jay0118@yonsei.ac.kr>
2021-07-28 10:10:22 +0900

    jupyter

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -10,7 +10,9 @@ echo "Download data, pretrained model weight"
 echo "########################################"
 echo ""
 
+mkdir -p ./saved_models/3DM
 gdown https://drive.google.com/uc?id=1kUtX1977ly9Kj5sdpcVl_lMPmwQTXlpN
+cp ./3DM_UNet_pretrained.pth ./saved_models/3DM/G_BA_26000.pth
 echo "Downloaded model weight"
 gdown https://drive.google.com/uc?id=1gmi_bjMFJ-0dfAHURKrhI15W7VQmXpmZ
 echo "Downloaded data"
